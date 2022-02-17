578f00190bde0a8ffcadccb912e104dbd1a68853
Bipin Khatiwada <bipinkh91@gmail.com>
2022-02-17 15:47:16 +0200

    Document the functions

--- services/redis_connector.py
+++ services/redis_connector.py
@@ -5,15 +5,32 @@ import json
 class RedisConnector:
 
     def __init__(self, hostname, port, password=None):
+        """
+        Default constructor
+        :param hostname: hostname of redis
+        :param port: redis server port
+        :param password: redis server password
+        """
         self.redis = redis.Redis(host=hostname, port=port, password=password, db=1)
 
     def put(self, key, value):
+        """
+        Insert value in the redis db
+        :param key: key
+        :param value: dictionary value
+        :return: None
+        """
         json_value = ""
         if value:
             json_value = json.dumps(value)
         self.redis.set(key, json_value)
 
     def get(self, key):
+        """
+        Get value from redis
+        :param key: key
+        :return: dictionary value
+        """
         value = self.redis.get(key)
         if value:
             json_value = value.decode("utf-8")
