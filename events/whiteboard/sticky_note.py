3b364dc2e16fddece094763685687b1996f5446e
Hussam Alkhafaji <sam66ish@localhost.localdomain>
2022-03-07 18:28:36 +0200

    Started implementing sticky_note.py

--- events/whiteboard/sticky_note.py
+++ events/whiteboard/sticky_note.py
@@ -1,7 +1,15 @@
 from events.whiteboard.whiteboard_event import WhiteboardEvent
 
+from services.redis_connector import RedisConnector
 
 class StickyNoteWhiteboardEvent(WhiteboardEvent):
     def __init__(self, text: str, **kwargs):
         super().__init__(**kwargs)
         self.text = text
+
+    def has_perm(self) -> bool:
+        users = self.room_users
+        return self.user_id in users
+
+    def handle(self):
+        return self.whiteboarding.redis_connector.insert_event(self.room_id, vars(self))
