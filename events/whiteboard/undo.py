41fc5881068fd23464b841802cee1c7235d1bd5c
sepehr javid <javid.sepehr77@gmail.com>
2022-03-21 17:23:41 +0330

    add last event id to the undo

--- events/whiteboard/undo.py
+++ events/whiteboard/undo.py
@@ -7,10 +7,11 @@ from events.whiteboard.whiteboard_event import WhiteboardEvent
 class UndoWhiteboardEvent(WhiteboardEvent):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.last_event_id = None
 
     async def handle(self):
-        last_event_id = self.whiteboarding.redis_connector.get_last_event_id(self.room_id)
-        self.whiteboarding.redis_connector.remove_event(last_event_id)
+        self.last_event_id = self.whiteboarding.redis_connector.get_last_event_id(self.room_id)
+        self.whiteboarding.redis_connector.remove_event(self.last_event_id)
         await self.client_socket.send(json.dumps({"status": 200, "event": self.to_dict(), "uuid": self.uuid}))
         await self.redistribute_event()
 
@@ -20,4 +21,5 @@ class UndoWhiteboardEvent(WhiteboardEvent):
     def to_dict(self) -> dict:
         parent_dict = super().to_dict()
         parent_dict['type'] = EventType.UNDO
+        parent_dict['last_event_id'] = self.last_event_id
         return parent_dict
