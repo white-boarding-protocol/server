da0a58ffc553197259e7d47e05bea83c4208c801
Bipin Khatiwada <bipinkh91@gmail.com>
2022-03-21 13:53:54 +0200

    Update image and sticky events

--- events/whiteboard/sticky_note.py
+++ events/whiteboard/sticky_note.py
@@ -16,7 +16,7 @@ class StickyNoteWhiteboardEvent(WhiteboardEvent):
         elif self.action == EventAction.REMOVE:
             self.whiteboarding.redis_connector.remove_event(self.room_id, self.event_id)
         elif self.action == EventAction.EDIT:
-            self.whiteboarding.redis_connector.edit_event(self.room_id, self.to_dict())
+            self.whiteboarding.redis_connector.edit_event(self.event_id, self.to_dict())
         await self.client_socket.send(json.dumps({"status": 200, "event": self.to_dict(), "uuid": self.uuid}))
         await self.redistribute_event()
 
