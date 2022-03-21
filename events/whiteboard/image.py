a5fe3fcb18a6e038657c593323fb270350ad634c
Hussam Alkhafaji <h-ssom1997@hotmail.com>
2022-03-21 12:41:09 +0200

    Fixed typo

--- events/whiteboard/image.py
+++ events/whiteboard/image.py
@@ -20,7 +20,7 @@ class ImageWhiteboardEvent(WhiteboardEvent):
             # Remove image last
             self.whiteboarding.redis_connector.remove_event(self.room_id, self.event_id)
         elif self.action == EventAction.EDIT:
-            self.event_id = self.whiteboarding.redis_connector.edit_event(self.room_id, self.to_dict)
+            self.event_id = self.whiteboarding.redis_connector.edit_event(self.room_id, self.to_dict())
         await self.client_socket.send(json.dumps({"status": 200, "event": self.to_dict(), "uuid": self.uuid}))
         await self.redistribute_event()
 
